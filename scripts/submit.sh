0c7c4951da9a7d4041bf10ceb5b341b808a065f5
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2026-09-14 19:50:32 +0000

    Add a help comment in submit script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,5 +1,8 @@
 #ord_soumet= -cpus 128 -cm 256G -t 21600 -jn surgemip-twl -mach ppp8
 
+
+# to pass the script path use: ord_soumet ./scripts/submit.sh -args ./run_all_surge.sh
+
 cd  ~/Python/surgemip
 . ~/.profile_python3
 
